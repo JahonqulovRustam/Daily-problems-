72c4f3f6f103fd5994599be2872680a1672af899
JahonqulovRustam <jahonqulovr@gmail.com>
2026-02-04 21:20:53 +0500

    Update Masala #M090J.java

--- Masala #M090J.java
+++ Masala #M090J.java
@@ -5,27 +5,24 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-        long A = sc.nextLong();
-
-        String S = String.valueOf(A);
-        if(S.length() != N){
-            System.out.println("Uzunlik mos emas!");
-            return;
-        }
+        String S = sc.next();
 
         StringBuilder sb = new StringBuilder();
 
-        for (int i = 0; i < N; i++) {
-            char c = S.charAt(i);
-            if (c == '0' || c == '5' || c == '9') {
-                sb.append(c);
-            } else {
-                sb.append('8');
+        if(S.charAt(0)=='-'||N==0){
+          return;
+        }
+        else{
+            for (int i = 0; i < N; i++) {
+                char c = S.charAt(i);
+                if (c == '0' || c == '5' || c == '9') {
+                    sb.append(c);
+                } else {
+                    sb.append('8');
+                }
             }
         }
         System.out.println(sb.toString());
-
-
     }
 }
 
