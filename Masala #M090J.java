686bf729d31a3c620b2e2aaac73f9e571405e07b
JahonqulovRustam <jahonqulovr@gmail.com>
2026-02-04 23:29:31 +0500

    Update Masala #M090J.java

--- Masala #M090J.java
+++ Masala #M090J.java
@@ -7,18 +7,19 @@ public class Main {
         int numberLen = sc.nextInt();
         String numberStr = sc.next();
 
-        StringBuilder sb = new StringBuilder();
+        StringBuilder result = new StringBuilder();
 
         for (int i = 0; i < numberLen; i++) {
             char c = numberStr.charAt(i);
             if (c == '0' || c == '5' || c == '9') {
-                sb.append(c);
+                result.append(c);
             } else {
-                sb.append('8');
+                result.append('8');
             }
         }
-        System.out.println(sb);
+        System.out.println(result);
     }
 }
+
 
 
