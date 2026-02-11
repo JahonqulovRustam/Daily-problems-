0637a3217cd3131edd0c6cc0a1f095bf5a6931f9
JahonqulovRustam <jahonqulovr@gmail.com>
2026-02-12 01:04:24 +0500

    new version(problem-sum-of-series)

--- Main.java
+++ Main.java
@@ -1,24 +1,15 @@
-import java.math.BigInteger;
 import java.util.Scanner;
-
-import static java.lang.Math.pow;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int numElements = sc.nextInt(); // chunki index faqat int tipini oladi
-        final BigInteger value = BigInteger.valueOf(((long) pow(10, 9) + 7)); // value bu o'zgarmas qiymatda
-
-        long [] arr = new long[numElements];  // bu yerda o'sha n lar uchun array yarataman va qiymatlarni qabul qilaman
+        int numElements = sc.nextInt();
+        final long value = 1000000007;
+        
         for(int i = 0; i < numElements; i++) {
-            arr[i] = sc.nextLong();
+            long number = sc.nextLong();
+            long result = (number % value) * (number % value) % value ;
+            System.out.println(result);
         }
-
-        for(int i = 0; i < numElements; i++) {
-            BigInteger n = BigInteger.valueOf(arr[i]);  // bu yerda n, nechta hadni qo'shishimiz kerakligini ifodalayopti
-            BigInteger sum = n.multiply(n).mod(value);
-            System.out.println(sum);
-        }
-
     }
 }
