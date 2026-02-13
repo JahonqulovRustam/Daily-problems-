028d7f99941fa0615ce4e8040b40c365694ded3a
JahonqulovRustam <jahonqulovr@gmail.com>
2026-02-13 23:50:16 +0500

    Latest version of problem

--- Main.java
+++ Main.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
 
 public class Main {
+    private static final long MOD = 1_000_000_007;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int numElements = sc.nextInt();
-        final long value = 1000000007;
-        
-        for(int i = 0; i < numElements; i++) {
+
+        for (int i = 0; i < numElements; i++) {
             long number = sc.nextLong();
-            long result = (number % value) * (number % value) % value ;
+            long subResult = number % MOD;
+            long result = (subResult * subResult) % MOD;
             System.out.println(result);
         }
     }
